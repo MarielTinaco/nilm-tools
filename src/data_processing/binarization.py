88cc1c9f19ed9df6d00cdad8859af9b1c00d373f
Mariel Tinaco <mariel.tinaco@analog.com>
2024-03-04 20:40:17 +0800

    Add: Strategy pattern structure for Binarization

---
+++ src/data_processing/binarization.py
@@ -0,0 +1,71 @@
+__all___ = ["BinarizationContext", "ThresholdBinarization", "binarization"]
+
+import numpy as np
+from enum import Enum
+from typing import Optional, Union, List
+from abc import ABC, abstractmethod
+
+class BinarizationTypes(Enum):
+        THRESHOLD = "threshold"
+
+class BinarizationContext(object):
+    
+        def __init__(self, 
+                     strategy : Optional[Union["Binarization", str]] = None,
+                     *args, **kwargs):
+                
+
+                if isinstance(strategy, str):
+                        if strategy == "threshold":
+                                strategy = ThresholdBinarization(threshold=kwargs.get("threshold") or 10)
+
+                        else:
+                                print(f"""
+                                WARNING: '{strategy}' mode not available. Please select between ["threshold"].
+                                Defaulting to Threshold Binarization mode.
+                                """)
+                                strategy = ThresholdBinarization(threshold=kwargs.get("threshold") or 10)
+
+                self._strategy = strategy
+
+        def __call__(self, data: np.ndarray) -> np.ndarray:
+                return self._strategy.binarize(data=data)
+
+
+class Binarization(ABC):
+
+        @abstractmethod
+        def binarize(self, data: np.ndarray) -> np.ndarray:
+                raise NotImplementedError
+
+
+class ThresholdBinarization(Binarization):
+
+        def __init__(self, threshold, *args, **kwargs):
+                self._threshold = threshold
+
+        
+        def binarize(self, data: np.ndarray) -> np.ndarray:
+                """[summary]
+                
+                Arguments:
+                        data {[type]} -- [description]
+                        threshold {[type]} -- [description]
+                
+                Returns:
+                        [type] -- [description]
+                """
+                return np.where(data>= self._threshold,1,0).astype(int)
+        
+
+def binarization(data, mode="threshold", **kwargs):
+
+        if mode not in BinarizationTypes._member_names_:
+                mode = "threshold"
+
+        if mode == "threshold":
+                thresh = kwargs.get("threshold")
+        
+        binarizer = BinarizationContext(strategy=mode, threshold=thresh)
+
+        return binarizer(data)
