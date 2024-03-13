4675bcadd6bcca2a6819a288a7f145aa5aa0f4db
ivangilmercano <ivangil.mercano@analog.com>
2024-03-13 21:12:22 +0800

    fix for normalize.py
    Signed-off-by: ivangilmercano <ivangil.mercano@analog.com>

--- src/data_processing/normalize.py
+++ src/data_processing/normalize.py
@@ -1,31 +1,28 @@
+__all___ = ["MinMaxScalling", "ModifiedMinMaxScaling", "StandardScaling"]
+
 from abc import ABC, abstractmethod
+import numpy as np
+from enum import Enum
+
 
 class NormalizationType(Enum):
     MINMAX = "minmax"
     MODMINMAX = "mod_minmax"
     STANDARD = "standard"
 
+
 class NormalizationContext(object):
-
-    def __init__(self, 
-                 strategy: Optional[Union["NormalizeStrategy", str]] = None):
-
-        if strategy is None:
-            strategy = MinMaxScaling(data)
-        
-        elif isinstance(strategy, str):
-            strategy = NormalizationProfiles[NormalizationType(strategy)](data)
-
+    def __init__(self, strategy: "NormalizeStrategy"):
         self.strategy = strategy
-    
+
     def normalize(self, data):
         return self.strategy.normalize(data)
 
     def denormalize(self, data):
         return self.strategy.denormalize(data)
 
-class NormalizeStrategy(ABC):
 
+class NormalizeStrategy(ABC):
     @abstractmethod
     def normalize(self, data) -> np.ndarray:
         pass
@@ -34,52 +31,51 @@ class NormalizeStrategy(ABC):
     def denormalize(self, data) -> np.ndarray:
         pass
 
+
 class MinMaxScaling(NormalizeStrategy):
-    
     def __init__(self, min_val, max_val):
         self.min = min_val
         self.max = max_val
-    
+
     def normalize(self, data) -> np.ndarray:
-        norm_data = (data - self.min)/(self.max - self.min)
+        norm_data = (data - self.min) / (self.max - self.min)
         return norm_data
-    
+
     def denormalize(self, data) -> np.ndarray:
-        denorm_data = data * (self.max_val  - self.min_val) + self.min_val
+        denorm_data = data * (self.max_val - self.min_val) + self.min_val
         return denorm_data
-    
 
 
 class ModifiedMinMaxScaling(NormalizeStrategy):
-    
     def __init__(self, min_val, max_val):
         self.min = min_val
         self.max = max_val
-    
+
     def normalize(self, data) -> np.ndarray:
-        norm_data = 2*((data - self.min)/(self.max - self.min))-1
+        norm_data = 2 * ((data - self.min) / (self.max - self.min)) - 1
         return norm_data
-    
+
     def denormalize(self, data) -> np.ndarray:
-        denorm_data = (((data + 1)*(self.max - self.min))/2) + self.min
+        denorm_data = (((data + 1) * (self.max - self.min)) / 2) + self.min
         return denorm_data
 
+
 class StandardScaling(NormalizeStrategy):
-    
-    def __init__(self, std_val, std_val):
+    def __init__(self, mean_val, std_val):
         self.std = std_val
         self.mean = mean_val
-    
+
     def normalize(self, data) -> np.ndarray:
         norm_data = (data - self.mean) / self.std
         return norm_data
-    
+
     def denormalize(self, data) -> np.ndarray:
         denorm_data = (data * self.std) + self.mean
         return denorm_data
 
+
 NormalizationProfiles = {
-    NormalizationType.MINMAX : MinMaxScalling,
-    NormalizationType.MODMINMAX : ModifiedMinMaxScalling,
-    NormalizationType.STANDARD : StandardScalling
+    NormalizationType.MINMAX: MinMaxScalling,
+    NormalizationType.MODMINMAX: ModifiedMinMaxScalling,
+    NormalizationType.STANDARD: StandardScalling,
 }
