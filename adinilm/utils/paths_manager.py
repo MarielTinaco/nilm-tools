c4b1c909ff2ed6b6bf78a4ec5598aa0c5afebf31
Mariel Tinaco <mariel.tinaco@analog.com>
2024-06-06 14:56:16 +0800

    Update: paths

--- adinilm/utils/paths_manager.py
+++ adinilm/utils/paths_manager.py
@@ -4,5 +4,6 @@ from pathlib import Path
 dirname = os.path.dirname(__file__)
 
 UKDALE_H5_PATH = os.path.join(dirname, '../../data/UKDALE/UKDALE.h5')
-SRC_DIR = Path(os.path.join(dirname, "../../src"))
+SRC_DIR = Path(os.path.join(dirname, "../../adinilm"))
 PROFILES_DIR = Path(os.path.join(dirname, "../../profiles"))
+DATA_DIR = Path(os.path.join(dirname, "../../data"))
