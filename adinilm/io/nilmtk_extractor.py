be3528a1820aebf688ba91baf767ed3f146d3983
Mariel Tinaco <mariel.tinaco@analog.com>
2024-07-01 15:15:08 +0800

    Update: Add activations to columns

--- adinilm/io/nilmtk_extractor.py
+++ adinilm/io/nilmtk_extractor.py
@@ -6,6 +6,8 @@ import nilmtk
 
 class NilmtkSubsectionExtractor:
 
+        COLUMNS = ["power_series", "on_power_threshold", "activations"]
+
         def __init__(self, dataset_path, params=None):
                 self.dataset_path = Path(dataset_path)
                 self.params = params
@@ -17,14 +19,20 @@ class NilmtkSubsectionExtractor:
                 ds = nilmtk.DataSet(self.dataset_path)
                 ds.set_window(start=params["start_time"], end=params["end_time"])
                 elec = ds.buildings[params["building"]].elec
-                apps_data = [[elec[app].power_series_all_data(sample_period=params["sample_period"],
-                                                              resample=params["resample"]),
-                              elec[app].on_power_threshold()]
-                              for app in params["appliances"]]
+
+                apps_data = []
+                for app in params["appliances"]:
+                        per_app = []
+                        per_app.append(elec[app].power_series_all_data(sample_period=params["sample_period"],
+                                                                       resample=params["resample"]))
+                        per_app.append(elec[app].on_power_threshold())
+                        per_app.append(elec[app].get_activations())
+                        
                 apps_data.append([elec.mains().power_series_all_data(sample_period=params["sample_period"],
                                                                      resample=params["resample"]),
+                                  np.NaN,
                                   np.NaN])
-                self.df = pd.DataFrame(apps_data, index=params["appliances"] + ["site meter"], columns=["power_series", "on_power_threshold"])
+                self.df = pd.DataFrame(apps_data, index=params["appliances"] + ["site meter"], columns=self.COLUMNS)
                 return self
 
         def to_pickle(self, *args, **kwargs):
