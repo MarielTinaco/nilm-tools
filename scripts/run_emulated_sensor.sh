ea2fe9e1f321bd8fdd908c5f89d4b20def4c3ffe
Mariel Tinaco <mariel.tinaco@analog.com>
2024-07-22 16:45:18 +0800

    add: sensor emulator script

---
+++ scripts/run_emulated_sensor.sh
@@ -0,0 +1,3 @@
+cd adinilm/firmware/emulator
+
+python sensor_emulator.py --port COM20 --test-set ../../../data/emu/test_set.csv --test-set-rms ../../../data/emu/test_set_rms.csv --test-set-states ../../../data/emu/test_set_states.csv --liveplots --test-start-idx 70
