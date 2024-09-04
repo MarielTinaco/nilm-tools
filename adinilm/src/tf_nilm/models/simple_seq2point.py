4aedc1f648944f6d3cf8d3c60fd87f6d146c0def
Mariel Tinaco <mariel.tinaco@analog.com>
2024-09-04 13:39:30 +0800

    Update: Convert from 2D convolution to 1D

--- adinilm/src/tf_nilm/models/simple_seq2point.py
+++ adinilm/src/tf_nilm/models/simple_seq2point.py
@@ -27,33 +27,34 @@ def create_model(input_window_length):
         model = keras.Model(inputs=input_layer, outputs=[output_layer_1, output_layer_2])
         return model
 
+
 def create_resnet_model(input_window_length, dropout=0.2):
     
-        init = keras.initializers.GlorotNormal(seed=None)
+        init = keras.initializers.HeNormal(seed=None)
 
         input_layer = keras.layers.Input(shape=(input_window_length,))
-        reshape_layer = keras.layers.Reshape((1, input_window_length, 1))(input_layer)
-        
-        conv1 = keras.layers.Conv2D(filters=32, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
-                                        kernel_initializer=init)(reshape_layer)
+        input_layer = keras.layers.Reshape((input_window_length,1))(input_layer)
+
+        conv1 = keras.layers.Conv1D(filters=32, kernel_size=3, strides=1, padding="causal", activation="relu",
+                                        kernel_initializer=init)(input_layer)
         conv1 = keras.layers.BatchNormalization()(conv1)
-        conv2 = keras.layers.Conv2D(filters=32, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv2 = keras.layers.Conv1D(filters=32, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv1)
         conv2 = keras.layers.BatchNormalization()(conv2)
-        conv3 = keras.layers.Conv2D(filters=32, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv3 = keras.layers.Conv1D(filters=32, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv2)
         resid_1 = keras.layers.Add()([conv2, conv3])
         resid_1 = keras.layers.ReLU()(resid_1)
         resid_1 = keras.layers.BatchNormalization()(resid_1)
 
-        conv4 = keras.layers.Conv2D(filters=48, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
-                                        kernel_initializer=init)(reshape_layer)
+        conv4 = keras.layers.Conv1D(filters=48, kernel_size=3, strides=1, padding="causal", activation="relu",
+                                        kernel_initializer=init)(resid_1)
         conv4 = keras.layers.BatchNormalization()(conv4)
-        conv5 = keras.layers.Conv2D(filters=48, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv5 = keras.layers.Conv1D(filters=48, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv4)
         conv5 = keras.layers.BatchNormalization()(conv5)
-        conv5 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv5)
-        conv6 = keras.layers.Conv2D(filters=48, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv5 = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(conv5)
+        conv6 = keras.layers.Conv1D(filters=48, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv5)
         conv6 = keras.layers.BatchNormalization()(conv6)
         resid_2 = keras.layers.Add()([conv5, conv6])
@@ -62,14 +63,14 @@ def create_resnet_model(input_window_length, dropout=0.2):
 
         dropout_layer_0 = keras.layers.Dropout(dropout)(resid_2)
 
-        conv7 = keras.layers.Conv2D(filters=64, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv7 = keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(dropout_layer_0)
         conv7 = keras.layers.BatchNormalization()(conv7)
-        conv8 = keras.layers.Conv2D(filters=64, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv8 = keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv7)
         conv8 = keras.layers.BatchNormalization()(conv8)
-        conv8 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv8)
-        conv9 = keras.layers.Conv2D(filters=64, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv8 = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(conv8)
+        conv9 = keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv8)
         conv9 = keras.layers.BatchNormalization()(conv9)
         resid_3 = keras.layers.Add()([conv8, conv9])
@@ -78,14 +79,14 @@ def create_resnet_model(input_window_length, dropout=0.2):
 
         dropout_layer_1 = keras.layers.Dropout(dropout)(resid_3)
 
-        conv10 = keras.layers.Conv2D(filters=96, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv10 = keras.layers.Conv1D(filters=96, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(dropout_layer_1)
         conv10 = keras.layers.BatchNormalization()(conv10)
-        conv11 = keras.layers.Conv2D(filters=96, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv11 = keras.layers.Conv1D(filters=96, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv10)
         conv11 = keras.layers.BatchNormalization()(conv11)
-        conv11 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv11)
-        conv12 = keras.layers.Conv2D(filters=96, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv11 = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(conv11)
+        conv12 = keras.layers.Conv1D(filters=96, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv11)
         conv12 = keras.layers.BatchNormalization()(conv12)
         resid_4 = keras.layers.Add()([conv11, conv12])
@@ -94,16 +95,16 @@ def create_resnet_model(input_window_length, dropout=0.2):
 
         dropout_layer_2 = keras.layers.Dropout(dropout)(resid_4)
 
-        conv13 = keras.layers.Conv2D(filters=128, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv13 = keras.layers.Conv1D(filters=128, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(dropout_layer_2)
-        conv13 = keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv13)
+        conv13 = keras.layers.AveragePooling1D(pool_size=2, strides=2, padding='valid')(conv13)
         conv13 = keras.layers.BatchNormalization()(conv13)
 
-        conv14 = keras.layers.Conv2D(filters=96, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv14 = keras.layers.Conv1D(filters=96, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv13)
         conv14 = keras.layers.BatchNormalization()(conv14)
 
-        conv15 = keras.layers.Conv2D(filters=64, kernel_size=(3, 1), strides=(1, 1), padding="same", activation="relu",
+        conv15 = keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding="causal", activation="relu",
                                         kernel_initializer=init)(conv14)
         conv15 = keras.layers.BatchNormalization()(conv15)
 
